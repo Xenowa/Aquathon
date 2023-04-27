c7c426a5ed758124044fc4d7d9c1a15864084e90
Xenowa <xenocodecs@gmail.com>
2023-04-27 17:21:12 +0530

    Completing CI/CD Setup

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/Aquathon/", // Required to tell github to check on it's repository
   plugins: [react()],
 })
