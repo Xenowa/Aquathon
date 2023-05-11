25c404f54baf7a50bb0a6acd6984c3821323c9b2
Xenowa <xenocodecs@gmail.com>
2023-05-11 19:29:49 +0530

    Update: Setting up title partner

--- src/components/Partners.jsx
+++ src/components/Partners.jsx
@@ -3,7 +3,7 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
-import codegenlogo from "../assets/Codegen logo.png";
+import codegenlogo from "../assets/Codegen-logo.png";
 
 export default function Partners() {
     return (
